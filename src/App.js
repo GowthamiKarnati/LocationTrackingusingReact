4e5db6e962a39d176861a6601f42ba2e6c59fdbd
Gowthami karnati <karnatigowthami43@gmail.com>
2024-08-06 12:55:08 +0530

    nbw

--- src/App.js
+++ src/App.js
@@ -1,212 +1,4 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// import axios from 'axios';
-// import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import L from 'leaflet';
 
-// // Fix the default icon issue with leaflet
-// delete L.Icon.Default.prototype._getIconUrl;
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-//   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-//   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-// });
-
-// function App() {
-//   const [markers, setMarkers] = useState([]);
-//   const markerRefs = useRef([]);
-//   const [center, setCenter] = useState([20.5937, 78.9629])
-//   console.log("Center", center);
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('https://backendforpnf.vercel.app/getgps');
-        
-//         const data = response.data.data
-//         console.log(typeof data);
-//         const latAvg = data.reduce((prev, curr, index)=>prev+parseFloat(curr.latitude), 0 )/data.length
-//         const lonAvg = data.reduce((prev, curr, index)=>prev+parseFloat(curr.longitude), 0)/data.length
-//         //console.log(latAvg, lonAvg)
-//         setCenter([17.4065, 78.4772])
-//         setMarkers(response.data.data);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const handleMouseOver = (index) => {
-//     markerRefs.current[index].openPopup();
-//   };
-
-//   const handleMouseOut = (index) => {
-//     markerRefs.current[index].closePopup();
-//   };
-
-//   return (
-//     <div>
-//       <MapContainer center={center} zoom={13} style={{ height: '800px', width: '100%' }}>
-//         <TileLayer
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         />
-//         {markers.map((marker, index) => (
-//           <Marker
-//             key={index}
-//             position={[marker.latitude, marker.longitude]}
-//             ref={(el) => (markerRefs.current[index] = el)}
-//             eventHandlers={{
-//               mouseover: () => handleMouseOver(index),
-//               mouseout: () => handleMouseOut(index),
-//             }}
-//             riseOnHover={true}
-//           >
-//             <Popup>
-//               <div>
-//                 <h3>User: {marker['user name']}</h3>
-//                 <p>Latitude: {marker.latitude}</p>
-//                 <p>Longitude: {marker.longitude}</p>
-//               </div>
-//             </Popup>
-//           </Marker>
-//         ))}
-//       </MapContainer>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import React, { useState, useEffect, useRef } from 'react';
-// import axios from 'axios';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-// import L from 'leaflet';
-// import { EditControl } from 'react-leaflet-draw';
-// import 'leaflet-draw/dist/leaflet.draw.css';
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css';
-
-
-// // Fix the default icon issue with leaflet
-// delete L.Icon.Default.prototype._getIconUrl;
-
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-//   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-//   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-// });
-
-// function App() {
-//   const [markers, setMarkers] = useState([]);
-//   const markerRefs = useRef([]);
-//   //const [center, setCenter] = useState([17.4065, 78.4772]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('https://backendforpnf.vercel.app/getgps');
-//         const data = response.data.data;
-
-//         // Calculate average coordinates
-//         const latAvg = data.reduce((prev, curr) => prev + parseFloat(curr.latitude), 0) / data.length;
-//         const lonAvg = data.reduce((prev, curr) => prev + parseFloat(curr.longitude), 0) / data.length;
-//         console.log(latAvg, lonAvg)
-//         // Update center to average coordinates
-//         //setCenter([latAvg, lonAvg]);
-         
-//         // Update markers
-//         setMarkers(data);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []); // Empty dependency array ensures useEffect runs only once on mount
-
-//   const handleMouseOver = (index) => {
-//     markerRefs.current[index]?.openPopup();
-//   };
-
-//   const handleMouseOut = (index) => {
-//     markerRefs.current[index]?.closePopup();
-//   };
-//   const getUsernameColor = (username) => {
-//         switch (username.toLowerCase()) {
-//           case 'riktam test2':
-//             return 'blue';
-//           case 'ramchandra rotte':
-//             return 'red';
-//           case 'maula shaikh':
-//             return 'green';
-//           case 'abdul shaikh':
-//             return 'purple';
-//           case 'test test':
-//             return 'orange';
-//           case 'pratibha finance':
-//             return 'cyan';
-//           case 'sohail shaikh':
-//             return 'yellow';
-//           case 'shingshetty shingshetty':
-//             return 'pink'; 
-//           default:
-//             return 'gray'; // Default color for unknown usernames
-//         }
-//       };
-    
-//       const getMarkerIcon = (username) => {
-//         const color = getUsernameColor(username);
-//         const iconUrl = `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`;
-        
-//         // Use default icon if the specified icon is not available
-//         const defaultIconUrl = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png';
-//         const markerIcon = new L.Icon({
-//           iconUrl: iconUrl.length > 0 ? iconUrl : defaultIconUrl,
-//           iconSize: [25, 41],
-//           iconAnchor: [12, 41],
-//           popupAnchor: [1, -34],
-//           shadowSize: [41, 41],
-//         });
-//         return markerIcon;
-//       };
-      
-//   return (
-//     <div>
-//       <MapContainer center={[17.4065, 78.4772]} zoom={13} style={{ height: '800px', width: '100%' }}>
-//         <TileLayer
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         />
-//         {markers.slice(-5).map((marker, index) => (
-//           <Marker
-//             key={index}
-//             position={[parseFloat(marker.latitude), parseFloat(marker.longitude)]}
-//             ref={(el) => (markerRefs.current[index] = el)}
-//             eventHandlers={{
-//               mouseover: () => handleMouseOver(index),
-//               mouseout: () => handleMouseOut(index),
-//             }}
-//             riseOnHover={true}
-//             icon={getMarkerIcon(marker['user name'])}
-//           >
-//             <Popup>
-//               <div>
-//                 <h3>User: {marker['user name']}</h3>
-//                 <p>Latitude: {marker.latitude}</p>
-//                 <p>Longitude: {marker.longitude}</p>
-//               </div>
-//             </Popup>
-//           </Marker>
-//         ))}
-//       </MapContainer>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
@@ -222,7 +14,7 @@ const MyControlComponent = ({ onDateChange, selectedDate }) => {
   const map = useMap();
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(true);
-  
+  console.log("newDate", date);
   const maxDate = new Date();
   maxDate.setHours(0, 0, 0, 0);
   
@@ -295,11 +87,19 @@ function App() {
       try {
         const response = await axios.get('https://backendforpnf.vercel.app/getgps');
         const data = response.data.data;
-
+        const lastFiveRecords = data.slice(-5);
+        console.log(lastFiveRecords)
+        
         // Filter data based on selected date
         const filteredData = data.filter(item => {
-          const itemDate = new Date(item.timestamp); // Assuming timestamp is the date field
-          return itemDate.toDateString() === selectedDate.toDateString();
+          const itemDate = new Date(item.timestamp);
+          // Normalize itemDate to midnight for comparison
+          itemDate.setHours(0, 0, 0, 0);
+          // Normalize selectedDate to midnight for comparison
+          const normalizedSelectedDate = new Date(selectedDate);
+          normalizedSelectedDate.setHours(0, 0, 0, 0);
+
+          return itemDate.getTime() === normalizedSelectedDate.getTime();
         });
 
         if (filteredData.length > 0) {
@@ -345,11 +145,11 @@ function App() {
       case 'test test':
         return 'orange';
       case 'pratibha finance':
-        return 'cyan';
+        return 'orange';
       case 'sohail shaikh':
         return 'yellow';
       case 'shingshetty shingshetty':
-        return 'pink';
+        return 'black';
       default:
         return 'gray'; // Default color for unknown usernames
     }
